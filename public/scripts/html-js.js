987a5e5b85839bd9a3e334e6779df76743e645db
Adhitama Fikri <adhitamafikri.dev@gmail.com>
2022-01-23 17:39:32 +0700

    feat: html and js basic

--- public/scripts/html-js.js
+++ public/scripts/html-js.js
@@ -1,1 +1,17 @@
-console.log('welcome!!')
+console.log("welcome!!");
+
+function invokeAlert() {
+  alert("Alert invoked");
+}
+
+function handleChange(evt) {
+  console.log(evt.value);
+}
+
+function handleMouseOver(evt) {
+  console.log(evt.classList.add('hovered'))
+}
+
+function handleMouseOut(evt) {
+  console.log(evt.classList.remove('hovered'))
+}
