c981da51dfc3864fc002578a26ab1bf8df8d2beb
Adhitama Fikri <adhitamafikri.dev@gmail.com>
2022-01-23 18:31:26 +0700

    feat: basic form handling

--- public/scripts/html-js.js
+++ public/scripts/html-js.js
@@ -4,14 +4,29 @@ function invokeAlert() {
   alert("Alert invoked");
 }
 
-function handleChange(evt) {
-  console.log(evt.value);
+function handleChange(elem) {
+  console.log(elem.value);
 }
 
-function handleMouseOver(evt) {
-  console.log(evt.classList.add('hovered'))
+function handleMouseOver(elem) {
+  console.log(elem.classList.add("hovered"));
 }
 
-function handleMouseOut(evt) {
-  console.log(evt.classList.remove('hovered'))
+/**
+ *
+ * @param {Element} elem
+ */
+function handleMouseOut(elem) {
+  console.log(elem.classList.remove("hovered"));
+}
+
+/**
+ *
+ * @param {Event} evt
+ */
+function handleSubmission(evt) {
+  evt.preventDefault();
+  const form = document.forms["test-form"];
+  console.log(form["name"].value);
+  console.log(form["email"].value);
 }
